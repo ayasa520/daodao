14b9cf275eec10c53bf491d6ff73cf3d27c40523
ayasa520 <55699692+ayasa520@users.noreply.github.com>
2021-06-25 13:40:20 +0800

    Update daodao.js

--- daodao.js
+++ daodao.js
@@ -172,19 +172,20 @@ var generateddHtml = array => {
 
             var dataTime = '<p class="datatime">'+array[i].date+'</p>'
             var decodedHTML = HTMLDecode( array[i].content);
-//             var scriptHTMLList = decodedHTML.match(/<script>.*<\/script>/);
-//             if(scriptHTMLList!=null)
-//                 scriptHTML += scriptHTMLList[0];
+            var scriptHTMLList = decodedHTML.match(/<script>.*<\/script>/);
+            if(scriptHTMLList!=null)
+                scriptHTML += scriptHTMLList[0];
            
-            result += `<li class="item"><div>`+ dataTime  + `<p class="datacont">`+decodedHTML+`</p><p class="datafrom"><small>`+ from_icon + decodeURIComponent(array[i].from) +`</small></p></div></li>`;
+            result += `<li class="item"><div>`+ dataTime  + `<p class="datacont">`+decodedHTML.replace(/<script>.*<\/script>/,"")+`</p><p class="datafrom"><small>`+ from_icon + decodeURIComponent(array[i].from) +`</small></p></div></li>`;
         }
     } else {
         result += '!{_p("aside.card_funds.zero")}';
     }
     result += '</div></ul></section>'
 
-//     var $dom = document.querySelector('#bber');
-    set_innerHTML('bber',result);
+    var $dom = document.querySelector('#bber');
+    $dom.innerHTML = result;
+    set_innerHTML('exec-script',scriptHTML);
     window.lazyLoadInstance && window.lazyLoadInstance.update();
     window.pjax && window.pjax.refresh($dom);
 }
